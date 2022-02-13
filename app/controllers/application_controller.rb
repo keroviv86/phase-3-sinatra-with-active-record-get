54dc8ca2e7d8267fd79f62de0472e5392caf32ce
Vivian Yim <keroviv86@gmail.com>
2022-02-13 16:11:02 -0600

    commit tree 4006aaf694dd26c40a3a7632088cb0c73d846b5c

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,25 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
   get '/games' do
     games = Game.all.order(:title).limit(10)
     games.to_json
   end
+
+  # use the :id syntax to create a dynamic route
+  # get '/games/:id' do
+  #   # look up the game in the database using its ID
+  #   game = Game.find(params[:id])
+  #   # send a JSON-formatted response of the game data
+  #   game.to_json
+  # end
+    # look up the game in the database using its ID
+    # send a JSON-formatted response of the game data
+  get '/games/:id' do
+    game = Game.find(params[:id])
   
+      # include associated reviews in the JSON response
+    game.to_json(include: { reviews: { include: :user } })
+  end
+
 end
